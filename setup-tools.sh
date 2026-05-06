fccac0b2d59db48a588e009b25ba34f991be3e62
Bel7phegor <phucan2370@gmail.com>
2026-05-06 15:06:14 +0700

    feat: create install terraform

---
+++ setup-tools.sh
@@ -0,0 +1,18 @@
+# Cài dependencies
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# Thêm HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+  gpg --dearmor | \
+  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Thêm repo
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+  https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+  sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Cài đặt
+sudo apt update && sudo apt-get install terraform
+
+# Kiểm tra version
+terraform -version
